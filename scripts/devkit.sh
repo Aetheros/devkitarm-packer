c7c749744f9364e285e23de2a48a9d3d82219815
Aetheros <aetheros@frisbeemen.de>
2016-06-09 06:14:23 +0200

    Hoping it builds now

--- scripts/devkit.sh
+++ scripts/devkit.sh
@@ -3,7 +3,7 @@ cd /home/vagrant/
 wget http://jaist.dl.sourceforge.net/project/devkitpro/Automated%20Installer/devkitARMupdate.pl
 chmod a+x devkitARMupdate.pl
 ./devkitARMupdate.pl
-cat <<EOF > /home/vagrant/.profile
+cat <<EOF >> /home/vagrant/.profile
 if [ -d '/home/vagrant/devkitPro' ] ; then
 	DEVKITPRO = 'home/vagrant/devkitPro'
 	DEVKITARM = '$DEVKITPRO/devkitARM'
