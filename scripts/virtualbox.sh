7f9c7377ba79b719310a74db65cceee65df1602e
Aetheros <aetheros@frisbeemen.de>
2016-06-09 23:43:49 +0200

    JEEEEESUS

--- scripts/virtualbox.sh
+++ scripts/virtualbox.sh
@@ -1,24 +1,13 @@
-# Without libdbus virtualbox would not start automatically after compile
-apt-get -y install --no-install-recommends libdbus-1-3
+#!/bin/bash
 
-# Install Linux headers and compiler toolchain
-apt-get -y install build-essential linux-headers-$(uname -r)
+# Bail if we are not running inside VirtualBox.
+if [[ `facter virtual` != "virtualbox" ]]; then
+    exit 0
+fi
 
-# The netboot installs the VirtualBox support (old) so we have to remove it
-service virtualbox-ose-guest-utils stop
-rmmod vboxguest
-apt-get purge -y virtualbox-ose-guest-x11 virtualbox-ose-guest-dkms virtualbox-ose-guest-utils
-apt-get install -y dkms
-
-# Install the VirtualBox guest additions
-VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
-VBOX_ISO=VBoxGuestAdditions_$VBOX_VERSION.iso
-mount -o loop $VBOX_ISO /mnt
-yes|sh /mnt/VBoxLinuxAdditions.run
-umount /mnt
-
-#Cleanup VirtualBox
-rm $VBOX_ISO
-
-shutdown -r now
-sleep 60
+mkdir -p /mnt/virtualbox
+mount -o loop /home/vagrant/VBoxGuest*.iso /mnt/virtualbox
+sh /mnt/virtualbox/VBoxLinuxAdditions.run
+ln -s /opt/VBoxGuestAdditions-*/lib/VBoxGuestAdditions /usr/lib/VBoxGuestAdditions
+umount /mnt/virtualbox
+rm -rf /home/vagrant/VBoxGuest*.iso
