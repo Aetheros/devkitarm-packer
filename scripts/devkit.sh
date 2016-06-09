0764450055409a4de97ec9ebdd50c761ae3e0a10
Aetheros <aetheros@frisbeemen.de>
2016-06-09 16:26:50 +0200

    Minor Changes, devkit script modified to finally work properly

--- scripts/devkit.sh
+++ scripts/devkit.sh
@@ -1,45 +1,45 @@
 #!/bin/bash
-cd /home/vagrant/
+cd /opt/
 wget http://jaist.dl.sourceforge.net/project/devkitpro/Automated%20Installer/devkitARMupdate.pl
 chmod a+x devkitARMupdate.pl
 ./devkitARMupdate.pl
 
-cat <<EOF >> /home/vagrant/.profile
-if [ -d '/home/vagrant/devkitPro' ] ; then
-	DEVKITPRO = 'home/vagrant/devkitPro'
-	DEVKITARM = '$DEVKITPRO/devkitARM'
-	export DEVKITPRO=$DEVKITPRO
-	export DEVKITARM=$DEVKITARM
-fi
-EOF
+echo "export DEVKITPRO=/opt/devkitPro" >> ~/.bashrc
+echo "export DEVKITARM=/opt/devkitPro/devkitARM" >> ~/.bashrc
+echo "export PATH=$PATH:/opt/devkitPro/devkitARM/bin" >> ~/.bashrc
+source ~/.bashrc
 
 rm *.tar.bz2
+rm devkitARMupdate.pl
 
 touch /usr/bin/update-devkit.sh
 chmod a+x /usr/bin/update-devkit.sh
 cat <<EOF > /usr/bin/update-devkit.sh
 #!/bin/bash
 echo 'updating devkit'
-cd /home/vagrant/
+cd /opt/
+wget http://jaist.dl.sourceforge.net/project/devkitpro/Automated%20Installer/devkitARMupdate.pl
+chmod a+x devkitARMupdate.pl
 ./devkitARMupdate.pl
 
 while getopts ":-:" opt; do
-    case ${opt} in
+    case \${opt} in
 		-)
-			case "${OPTARG}" in
+			case "\${OPTARG}" in
 				keep-files)
-					echo "Keeping downloaded files"
+					echo "Keeping downloaded files in opt directory, you should move them out of there."
 					exit 0
 					;;
 				*) 
-					echo "Invaild option: -$optarg. Proceeding to remove downloaded files!" >&2
+					echo "Invaild option: -\$optarg. Proceeding to remove downloaded files!" >&2
 					;;
 			esac
 		\?)
-			echo "Invaild option: -$optarg. Proceeding to remove downloaded files!" >&2
+			echo "Invaild option: -\$optarg. Proceeding to remove downloaded files!" >&2
 			;;
 	esac
 done
 echo 'removing downloaded files'
 rm *.tar.bz2
+rm devkitARMupdate.pl
 EOF
