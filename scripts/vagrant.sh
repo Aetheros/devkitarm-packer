3a502a8a82c9ab6957f44f45dbd7532f4f6596fa
Maximilian Jacobs <aetheros@frisbeemen.de>
2016-06-09 19:05:55 +0200

    Fidgeting with the buildscript a bit

--- scripts/vagrant.sh
+++ scripts/vagrant.sh
@@ -15,5 +15,7 @@ wget --no-check-certificate 'https://raw.githubusercontent.com/mitchellh/vagrant
 chmod 600 /home/vagrant/.ssh/authorized_keys
 chown -R vagrant /home/vagrant/.ssh
 
+echo 'Development Environment' > /etc/motd
+
 # Install NFS for Vagrant
 apt-get install -y nfs-common
