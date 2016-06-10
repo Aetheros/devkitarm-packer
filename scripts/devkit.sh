c702811c82b0df11e47025ea6cb0b5cb0e049fff
Aetheros <aetheros@frisbeemen.de>
2016-06-10 07:36:09 +0200

    That didn't work as planned

--- scripts/devkit.sh
+++ scripts/devkit.sh
@@ -6,16 +6,79 @@ echo "export DEVKITARM=/home/vagrant/devkitPro/devkitARM" >> ~/.bashrc
 echo "export PATH=$PATH:/home/vagrant/devkitPro/devkitARM/bin" >> ~/.bashrc
 source ~/.bashrc
 
-cd /usr/bin
-wget http://$HTTPSERV:$HTTPPORT/update-devkit
-wget http://$HTTPSERV:$HTTPPORT/update-portlibs
-wget http://$HTTPSERV:$HTTPPORT/update-sflibs
 
-chmod a+x update-devkit update-portlibs update-sflibs
+echo <<EOF > /usr/bin/update-devkit
+#!/bin/bash
 
-update-devkit
-update-portlibs
-update-sflibs
+mkdir -p /opt/devkit/
+cd /opt/devkit/
+wget http://jaist.dl.sourceforge.net/project/devkitpro/Automated%20Installer/devkitARMupdate.pl
+chmod a+x devkitARMupdate.pl
+./devkitARMupdate.pl
+
+echo "removing updater script to prevent accidental execution"
+
+rm devkitARMupdate.pl
+EOF
+
+
+echo <<EOF > /usr/bin/update-portlibs
+#!/bin/bash
+
+cd /home/vagrant/
+git clone "https://github.com/devkitPro/3ds_portlibs.git"
+cd "3ds_portlibs"
+git pull
+make clean
+make zlib
+make install-zlib
+make freetype 
+make libexif 
+make libjpeg-turbo 
+make libpng
+make jansson
+make libxmp-lite
+make sqlite
+make tinyxml2
+make install
+
+EOF
+
+echo <<EOF > /usr/bin/update-sflibs
+#!/bin/bash
+
+mkdir -p /home/vagrant/sflibs/
+cd /home/vagrant/sflibs/
+if ![ -d /home/vagrant/sf2dlib ]; then
+	git clone https://github.com/xerpi/sf2dlib.git
+	git clone https://github.com/xerpi/sftdlib.git
+	git clone https://github.com/xerpi/sfillib.git
+fi
+
+cd sf2dlib/
+git pull
+cd libsf2d
+
+make clean all install
+
+cd ../../sftdlib
+git pull
+cd libsftd
+
+make clean all install
+
+cd ../../sfillib
+git pull
+cd libsfil
+
+make clean all install
+EOF
+
+chmod a+x /usr/bin/update-devkit /usr/bin/update-portlibs /usr/bin/update-sflibs
+
+/usr/bin/update-devkit
+/usr/bin/update-portlibs
+/usr/bin/update-sflibs
 
 echo "chowning all files in vagrant folder"
 
