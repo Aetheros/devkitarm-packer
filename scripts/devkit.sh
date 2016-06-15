e283c24a57261f75dfd9915d955502640bab0118
Aetheros <aetheros@frisbeemen.de>
2016-06-15 03:07:21 +0200

    Added missing portlib

--- scripts/devkit.sh
+++ scripts/devkit.sh
@@ -13,8 +13,14 @@ export PATH=$PATH:/home/vagrant/devkitPro/devkitARM/bin
 cat <<EOF > /usr/bin/update-devkit
 #!/bin/bash
 
-mkdir -p /opt/devkit/
-cd /opt/devkit/
+if [ "\$USER" = "root" ]
+then
+	echo "Please do not run this script as root!"
+	exit
+fi
+
+mkdir -p /home/vagrant/devkitupdate
+cd /home/vagrant/devkitupdate
 wget http://jaist.dl.sourceforge.net/project/devkitpro/Automated%20Installer/devkitARMupdate.pl
 chmod a+x devkitARMupdate.pl
 ./devkitARMupdate.pl
@@ -80,8 +86,6 @@ make install
 cd ..
 rm -rf bannertool
 
-chown -R vagrant:vagrant /home/vagrant/
-
 EOF
 
 
@@ -99,6 +103,7 @@ make freetype
 make libexif 
 make libjpeg-turbo 
 make libpng
+make libconfig
 make jansson
 make libxmp-lite
 make sqlite
@@ -139,9 +144,118 @@ EOF
 
 chmod a+x /usr/bin/update-devkit /usr/bin/update-portlibs /usr/bin/update-sflibs
 
-/usr/bin/update-devkit
-/usr/bin/update-portlibs
-/usr/bin/update-sflibs
+
+mkdir -p /home/vagrant/devkitupdate
+cd /home/vagrant/devkitupdate
+wget http://jaist.dl.sourceforge.net/project/devkitpro/Automated%20Installer/devkitARMupdate.pl
+chmod a+x devkitARMupdate.pl
+./devkitARMupdate.pl
+
+echo "removing updater script to prevent accidental execution"
+
+rm devkitARMupdate.pl
+
+git clone https://github.com/smealum/ctrulib.git
+cd ctrulib
+git reset --hard #2810c4d3a
+cd libctru 
+make install
+rm -rf ctrulib
+
+git clone --recursive https://github.com/Kingcom/armips.git
+cd armips
+cmake ./
+make
+mv armips ~/devkitpro/devkitARM/bin
+cd ..
+rm -rf armips
+
+mkdir bin2c
+cd bin2c
+curl -L http://astronautlevel.webege.com/bin2c.c -o bin2c.c
+gcc bin2c.c -o bin2c
+mv bin2c ~/devkitpro/devkitARM/bin
+cd ..
+rm -r bin2c
+
+git clone --recursive https://github.com/profi200/Project_CTR.git
+cd Project_CTR/makerom
+make
+mv makerom ~/devkitpro/devkitARM/bin
+cd ../..
+rm -rf Project_CTR
+
+git clone --recursive https://github.com/fincs/picasso.git
+cd picasso
+./autogen.sh
+./configure
+make
+mv picasso ~/devkitpro/devkitARM/bin
+cd ../
+rm -rf picasso
+
+git clone --recursive https://github.com/Steveice10/citrus.git
+cd citrus
+make install
+cd ../
+rm -rf citrus
+
+git clone --recursive https://github.com/fincs/citro3d.git
+cd citro3d
+make install
+cd ../
+rm -rf citro3d
+
+git clone --recursive https://github.com/Steveice10/bannertool.git
+cd bannertool
+make install
+cd ..
+rm -rf bannertool
+
+cd /home/vagrant/
+git clone "https://github.com/devkitPro/3ds_portlibs.git"
+cd "3ds_portlibs"
+git pull
+make clean
+make zlib
+make install-zlib
+make freetype 
+make libexif 
+make libjpeg-turbo 
+make libpng
+make libconfig
+make jansson
+make libxmp-lite
+make sqlite
+make tinyxml2
+make install
+cd ..
+
+mkdir -p /home/vagrant/sflibs/
+cd /home/vagrant/sflibs/
+if ! [ -d '/home/vagrant/sf2dlib' ]; then
+	git clone https://github.com/xerpi/sf2dlib.git
+	git clone https://github.com/xerpi/sftdlib.git
+	git clone https://github.com/xerpi/sfillib.git
+fi
+
+cd sf2dlib/
+git pull
+cd libsf2d
+
+make clean all install
+
+cd ../../sftdlib
+git pull
+cd libsftd
+
+make clean all install
+
+cd ../../sfillib
+git pull
+cd libsfil
+
+make clean all install
 
 echo "chowning all files in vagrant folder"
 
